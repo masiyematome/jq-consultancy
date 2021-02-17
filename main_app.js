e795d62c172fbdb87ce956b10ccd5f1963ed6151
masiyematome <matomemasiye7@gmail.com>
2021-02-17 15:44:41 +0200

    Added a close button that closes the navbar links on click

--- main_app.js
+++ main_app.js
@@ -2,8 +2,16 @@
 const navTogglerButton = document.querySelector(".nav-toggler");
 const mainMenu = document.querySelector("#main-menu");
 const navbarLinks = document.querySelector(".navbar-links");
+const closeButton = document.querySelector(".close-button");
 
 navTogglerButton.addEventListener("click" , ()=> {
     mainMenu.classList.add("displaying");
     navbarLinks.classList.add("displaying");
+    closeButton.classList.add("displaying");
+})
+
+closeButton.addEventListener("click" , () => {
+    mainMenu.classList.remove("displaying");
+    navbarLinks.classList.remove("displaying");
+    closeButton.classList.remove("displaying");
 })

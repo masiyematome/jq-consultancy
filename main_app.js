6a669b993239dd950147010ceb0df9f3ec1d8600
masiyematome <matomemasiye7@gmail.com>
2021-02-18 12:30:46 +0200

    Changed the implementation of the toggle and close buttons and used the event delegation method

--- main_app.js
+++ main_app.js
@@ -1,4 +1,5 @@
 
+const navigationBar = document.querySelector(".navigation-bar");
 const navTogglerButton = document.querySelector(".nav-toggler");
 const mainMenu = document.querySelector("#main-menu");
 const navbarLinks = document.querySelector(".navbar-links");
@@ -6,18 +7,22 @@ const closeButton = document.querySelector(".close-button");
 const services = document.querySelectorAll(".service");
 const links = document.querySelectorAll("a");
 
-navTogglerButton.addEventListener("click" , ()=> {
-    mainMenu.classList.add("displaying");
-    navbarLinks.classList.add("displaying");
-    closeButton.classList.add("displaying");
-    navTogglerButton.classList.add("hiding");
-})
+navigationBar.addEventListener("click" , (event) => {
+    const clickedButton = event.target;
 
-closeButton.addEventListener("click" , () => {
-    mainMenu.classList.remove("displaying");
-    navbarLinks.classList.remove("displaying");
-    closeButton.classList.remove("displaying");
-    navTogglerButton.classList.remove("hiding");
+    if (clickedButton.classList.contains("nav-toggler")) {
+        mainMenu.classList.add("displaying");
+        navbarLinks.classList.add("displaying");
+        closeButton.classList.add("displaying");
+        navTogglerButton.classList.add("hiding");
+    }
+
+    else if(clickedButton.classList.contains("close-button")){
+        mainMenu.classList.remove("displaying");
+        navbarLinks.classList.remove("displaying");
+        closeButton.classList.remove("displaying");
+        navTogglerButton.classList.remove("hiding");
+    }
 })
 
 links.forEach(link => {

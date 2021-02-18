e437b3b3c3d5308ac8ca353c9b5522a9a2486c2a
masiyematome <matomemasiye7@gmail.com>
2021-02-18 12:15:20 +0200

    Wrote a block of code that hides the side navigation bar after a link has been clicked

--- main_app.js
+++ main_app.js
@@ -3,6 +3,8 @@ const navTogglerButton = document.querySelector(".nav-toggler");
 const mainMenu = document.querySelector("#main-menu");
 const navbarLinks = document.querySelector(".navbar-links");
 const closeButton = document.querySelector(".close-button");
+const services = document.querySelectorAll(".service");
+const links = document.querySelectorAll("a");
 
 navTogglerButton.addEventListener("click" , ()=> {
     mainMenu.classList.add("displaying");
@@ -16,4 +18,15 @@ closeButton.addEventListener("click" , () => {
     navbarLinks.classList.remove("displaying");
     closeButton.classList.remove("displaying");
     navTogglerButton.classList.remove("hiding");
+})
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        if (mainMenu.classList.contains("displaying")) {
+            mainMenu.classList.remove("displaying");
+            navbarLinks.classList.remove("displaying");
+            navTogglerButton.classList.remove("hiding");
+
+        }
+    })
 })
